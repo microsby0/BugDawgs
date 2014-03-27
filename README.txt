a518b9c8ebda61616108814fcc1e04c4b69b1d59
Michael Crosby <microsby0@gmail.com>
2014-03-27 12:29:49 -0400

    Moved code over to new file. Updated README

--- README.txt
+++
@@ -1,4 +0,0 @@
-Issues:
-	Need to put in visit end time instead of visit length
-	The check for whether a date has passed is removing the current day?
-	Probably more...
--- outreach_automation.py
+++ outreach_automation.py
@@ -4,13 +4,19 @@ import time
 from datetime import datetime
 import re
 
-def setUpHeaders(sheet):
-    sheet.update_acell('A1',"Events")
-    sheet.update_acell('B1',"Name\n****Denotes pick up/drop off****")
-    sheet.update_acell('C1',"Phone Number")
-    sheet.update_acell('D1',"810")
-    sheet.update_acell('E1',"Special Notes")
-    sheet.update_acell('F1',"Call if you need me 706-614-3328 (cell) 706-542-1238 (office)\nYou can also send a text, which I can access more easily in meetings and at home. Thanks and Good Luck! \nMarianne")
+def setUpHeaders():
+    student_sheet.update_acell('A1',"Events")
+    student_sheet.update_acell('B1',"Name\n****Denotes pick up/drop off****")
+    student_sheet.update_acell('C1',"Phone Number")
+    student_sheet.update_acell('D1',"810")
+    student_sheet.update_acell('E1',"Special Notes")
+    student_sheet.update_acell('F1',"Call if you need me 706-614-3328 (cell) 706-542-1238 (office)\nYou can also send a text, which I can access more easily in meetings and at home. Thanks and Good Luck! \nMarianne")
+    final_sheet.update_acell('A1',"Events")
+    final_sheet.update_acell('B1',"Name\n****Denotes pick up/drop off****")
+    final_sheet.update_acell('C1',"Phone Number")
+    final_sheet.update_acell('D1',"810")
+    final_sheet.update_acell('E1',"Special Notes")
+    final_sheet.update_acell('F1',"Call if you need me 706-614-3328 (cell) 706-542-1238 (office)\nYou can also send a text, which I can access more easily in meetings and at home. Thanks and Good Luck! \nMarianne")
 
 def clearCells(range,sheet):
     #A2:E number of rows
@@ -40,6 +46,86 @@ def populate():
         if curRow> 50:
             break
 
+def formatDates(rows):
+    for row in rows:
+        try:
+            date = datetime.strptime(row[2], "%m/%d/%Y")
+            row[2] = date
+        except ValueError:
+            #switch this to alert someone later
+            row[2] = datetime.now()
+
+def createSrcList(rows):
+    content         = ""
+    groupName       = ""
+    visitDate       = ""
+    visitTime       = ""
+    visitLength     = ""
+    contactName     = ""
+    contactPhone    = ""
+    contactEmail    = ""
+    location        = ""
+    address         = ""
+    groupSize       = ""
+    groupAge        = ""
+    tempList=[]
+
+    for row in src_rows:
+        groupName = row[1]
+
+        #Visit Date
+        visitDate = "Date: " + row[2].strftime("%A, %m/%d/%Y")
+
+        #Visit Time
+        #formats time to AM/PM 12 hour
+        try:
+            time_values = time.strptime(row[3], "%H:%M:%S")
+            visitTime = time.strftime("%I:%M %p", time_values)
+        except ValueError:
+            #switch this to alert someone later
+            time_values = time.strptime("12:30:34", "%H:%M:%S")
+            visitTime = time.strftime("%I:%M %p", time_values)
+
+        #Swap out for a field for visit end?
+        visitLength     = row[4]
+
+        contactName     = row[5]
+        contactPhone    = row[6]
+        contactEmail    = row[7]
+        location        = row[8]
+        address         = row[9]
+        groupSize       = row[10]
+        groupAge        = row[11]
+        comments        = row[12]
+
+        content = (
+            groupName + ":\n" +
+            visitDate + "\n\n" +
+            visitTime + "\n\n" +
+            "Contact: " + contactName + "\n" +
+            contactPhone + "\n" +
+            contactEmail + "\n\n" +
+            "Age Group: " + groupAge + "\n" +
+            "Number in Group: " + groupSize + "\n\n" +
+            location + "\n" +
+            address
+        )
+        tempList.append([content,"","","",comments])
+    return tempList
+
+def removeOutdatedEvents(masterList):
+    startRow = len(final_sheet.get_all_values()) + 1
+    startCol = 1
+    counter=0
+    for row in masterList:
+        if(datetime.now() > stringToDatetime(row[0])):
+            for cell in row:
+                final_sheet.update_cell(startRow,startCol,cell)
+                startCol+=1
+            startRow+=1
+            startCol=1
+            sortedMergedList.pop(counter)
+        counter+=1
 
 
 account = gspread.login(os.environ["GSPREAD_USERNAME"], os.environ["GSPREAD_PASSWORD"])
@@ -50,109 +136,41 @@ final_sheet = account.open("Final Dest").sheet1
 
 #populate()
 
-setUpHeaders(student_sheet)
-setUpHeaders(final_sheet)
+setUpHeaders()
 
+#get data from src sheet
 src_rows = src_sheet.get_all_values()
 src_rows.pop(0)
 
-content         = ""
-groupName       = ""
-visitDate       = ""
-visitTime       = ""
-visitLength     = ""
-contactName     = ""
-contactPhone    = ""
-contactEmail    = ""
-location        = ""
-address         = ""
-groupSize       = ""
-groupAge        = ""
-
-
 #formats dates in src sheet
-for row in src_rows:
-    try:
-        date = datetime.strptime(row[2], "%m/%d/%Y")
-        row[2] = date
-    except ValueError:
-        row[2] = datetime.now()
+formatDates(src_rows)
 
-# sorts src_sheet based on date of event
+#creates list of rows in src sheet
+src_sheet_row_list = createSrcList(src_rows)
 
-#puts info from src_sheet into variables
-src_sheet_row_list = []
-for row in src_rows:
-    groupName = row[1]
-
-    #Visit Date
-    visitDate = "Date: " + row[2].strftime("%A, %m/%d/%Y")
-
-    #Visit Time
-    #formats time to AM/PM 12 hour
-    try:
-        time_values = time.strptime(row[3], "%H:%M:%S")
-        visitTime = time.strftime("%I:%M %p", time_values)
-    except ValueError:
-        time_values = time.strptime("12:30:34", "%H:%M:%S")
-        visitTime = time.strftime("%I:%M %p", time_values)
-
-    #Swap out for a field for visit end?
-    visitLength     = row[4]
-
-    contactName     = row[5]
-    contactPhone    = row[6]
-    contactEmail    = row[7]
-    location        = row[8]
-    address         = row[9]
-    groupSize       = row[10]
-    groupAge        = row[11]
-    comments        = row[12]
-
-    content = (
-        groupName + ":\n" +
-        visitDate + "\n\n" +
-        visitTime + "\n\n" +
-        "Contact: " + contactName + "\n" +
-        contactPhone + "\n" +
-        contactEmail + "\n\n" +
-        "Age Group: " + groupAge + "\n" +
-        "Number in Group: " + groupSize + "\n\n" +
-        location + "\n" +
-        address
-    )
-    src_sheet_row_list.append([content,"","","",""])
-
+#gets list of rows from student sheet
 student_rows = student_sheet.get_all_values()
 
+#combines lists
 mergeList = student_rows + src_sheet_row_list
 mergeList.pop(0) #gets rid of header row
 
+#sorts the master list
 sortedMergedList = sorted(mergeList, key=lambda item: stringToDatetime(item[0]))
 
+#prints out size and dates in the master list before its sorted
+#print "Before: " + str(len(sortedMergedList))
 # for row in sortedMergedList:
 #    print stringToDatetime(row[0]).strftime("%m/%d/%Y")
 
-print "Before: " + str(len(sortedMergedList))
+removeOutdatedEvents(sortedMergedList)
 
-startRow = len(final_sheet.get_all_values()) + 1
-startCol = 1
-counter=0
-for row in sortedMergedList:
-    if(datetime.now() > stringToDatetime(row[0])):
-        for cell in row:
-            print "Start row: " + str(startRow)
-            print "Start col: " + str(startCol)
+#prints size and dates in master list after its sorted
+#print "After: " + str(len(sortedMergedList))
+# for row in sortedMergedList:
+#    print stringToDatetime(row[0]).strftime("%m/%d/%Y")
 
-            final_sheet.update_cell(startRow,startCol,cell)
-            startCol+=1
-        startRow+=1
-        startCol=1
-        sortedMergedList.pop(counter)
-    counter+=1
-
-print "After: " + str(len(sortedMergedList))
-
+#clears student sheet before it is updated
 rangeToClear = "A2:E" + str(len(student_rows))
 clearCells(rangeToClear,student_sheet)
 
@@ -164,6 +182,8 @@ for row in sortedMergedList:
     student_sheet.update_cell(currentRow,4,row[3]) #810s
     student_sheet.update_cell(currentRow,5,row[4]) #special notes
     currentRow += 1
+
+#clears all of src sheet
 rangeToClear="A2:N" + str(len(src_rows))
 clearCells(rangeToClear,src_sheet)
 
